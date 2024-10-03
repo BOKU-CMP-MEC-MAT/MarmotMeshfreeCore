ba084dcc0c947e278e3c383fa2b96e1a156a7747
Matthias Neuner <Matthias.Neuner@uibk.ac.at>
2024-10-03 16:10:22 +0200

    added new method

--- include/Marmot/MarmotMaterialPoint.h
+++ include/Marmot/MarmotMaterialPoint.h
@@ -64,5 +64,7 @@ public:
 
   virtual void getVertexCoordinates( double* coordinates ) const = 0;
 
+  virtual void getCenterDisplacement( double* displacement ) const = 0;
+
   virtual double getVolumeUndeformed() const = 0;
 };
